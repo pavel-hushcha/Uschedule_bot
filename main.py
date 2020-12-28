0f044249d9056c5f03a2fa1f100175a7b6652e4c
Pavel Hushcha <pavel.hushcha@gmail.com>
2020-12-28 16:28:09 +0300

    Add echo-messages function to telegram bot
    Just first commit in repo. Added the begin lines of bot's code and two functions: get_update() and send_message().
    get_update() returns chat_id, message_text and update_id of last message that typed into bot.
    
    send_message send the same message as answer to chat, if empty - I do not understand you.

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+URL = 'https://api.telegram.org/bot1377300202:AAHrZY0KokLcacdo_SriqiFLP6BIU5EyLFk/'
+
+
+def get_update():
+    url = URL + 'getupdates'
+    r = requests.get(url).json()
+    chat_id = r['result'][-1]['message']['chat']['id']
+    message_text = r['result'][-1]['message']['text']
+    update_id = r['result'][-1]['update_id']
+    return chat_id, message_text, update_id
+
+
+def send_message(chat_id, text='I do not understand you'):
+    url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
+    requests.get(url)
+
+
+chat_id, text, update_id = get_update()
+send_message(chat_id, text)
+
