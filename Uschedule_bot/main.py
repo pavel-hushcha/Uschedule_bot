e898841918b64e2be5557c74000f2ba7bc10a0b6
Pavel Hushcha <pavel.hushcha@gmail.com>
2021-01-26 17:36:23 +0300

    Replaced long pooling to webhook
    Removed long pooling, added webhook.

--- Uschedule_bot/main.py
+++ Uschedule_bot/main.py
@@ -63,10 +63,8 @@ def handle_text(message):
                      content_types=["text"])
 def handle_text(message):
     search_message = "Для поиска введите не менее 3 символов:"
-    # msgname = bot.send_message(message.chat.id, search_message)
-    bot.send_message(message.chat.id, "Для поиска введите не менее 3 символов:")
-    search_name_group(message)
-    # bot.register_next_step_handler(msgname, search_name_group)
+    msgname = bot.send_message(message.chat.id, search_message)
+    bot.register_next_step_handler(msgname, search_name_group)
 
 
 # search the name of group or teacher in the list of them
