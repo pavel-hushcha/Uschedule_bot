716add17cc8dbf7c028cd4ff092876964610ceea
Pavel Hushcha <pavel.hushcha@gmail.com>
2021-01-26 13:23:31 +0300

    Replaced long pooling to webhook
    Removed long pooling, added webhook.

--- Uschedule_bot/main.py
+++ Uschedule_bot/main.py
@@ -215,18 +215,18 @@ except (KeyboardInterrupt, SystemExit):
     pass
 
 
-@server.route("/" + token, methods=["POST"])
+@server.route('/' + token, methods=['POST'])
 def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utd-8"))])
-    return "POST", 200
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=f"{app_name}herokuapp.com/{token}")
-    return "CONNECTED", 200
+    bot.set_webhook(url='https://uschedule-bot.herokuapp.com/' + token)
+    return "!", 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
