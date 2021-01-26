33e494bf4cec6292b92c5053c9a46db9b1798a39
Pavel Hushcha <pavel.hushcha@gmail.com>
2021-01-26 11:56:26 +0300

    Replaced long pooling to webhook
    Removed long pooling, added webhook.

--- Uschedule_bot/main.py
+++ Uschedule_bot/main.py
@@ -229,4 +229,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT")))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
