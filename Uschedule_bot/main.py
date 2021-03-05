7ed4d19cf53cc3b0ad4a5a91561bb5e2ceadce64
Pavel Hushcha <pavel.hushcha@gmail.com>
2021-03-05 20:26:27 +0300

    Added manual set for subscribers
    Added possibility of manual setup subscriber's messages.

--- Uschedule_bot/main.py
+++ Uschedule_bot/main.py
@@ -325,8 +325,8 @@ def ringers():
 
 # scheduler of database updating at 14-30 UTC and ringer for subscribers from monday to saturday
 scheduler.add_job(update_base, trigger="cron", day_of_week='mon-sat', hour=14, minute=30)
-trigger_main = AndTrigger([IntervalTrigger(minutes=1), CronTrigger(day_of_week='mon-sat')])
-scheduler.add_job(ringers, trigger_main)
+# trigger_main = AndTrigger([IntervalTrigger(minutes=1), CronTrigger(day_of_week='mon-sat')])
+# scheduler.add_job(ringers, trigger_main)
 try:
     scheduler.start()
 except (KeyboardInterrupt, SystemExit):
